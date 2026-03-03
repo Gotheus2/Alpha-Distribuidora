c8e58f7aa685afad03b5d2e7cd6a701cc90ebd19
azui <enzogrutila@gmail.com>
2026-03-02 22:41:38 -0300

    Estrutura inicial do base.html e construcao da pagina de clientes

--- pagina_interna/views.py
+++ pagina_interna/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def cadastro_clientes(request):
+    return render(request, "cadastro_clientes.html")
--- setup/urls.py
+++ setup/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("login_usuario.urls")),
+    path("", include("pagina_interna.urls")),
 ]
