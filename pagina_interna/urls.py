8f2994a15253f19e4a1446444ec80515c90eb292
azui <enzogrutila@gmail.com>
2026-03-03 21:54:55 -0300

    finalizacao do front dos clientes

--- pagina_interna/urls.py
+++ pagina_interna/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('clientes/', views.cadastro_clientes, name="cadastro_clientes"),
+    path('clientes/', views.cadastro_clientes, name='cadastro_clientes'),
+    path('vendedores/', views.vendedores, name='vendedores'),
+    path('vendas/', views.vendas, name='vendas'),
+    path('produtos/', views.produtos, name='produtos'),
+    path('financeiro/', views.financeiro, name='financeiro'),
+    path('', views.dashboard, name='dashboard'),
 ]
--- pagina_interna/views.py
+++ pagina_interna/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 def cadastro_clientes(request):
     return render(request, "cadastro_clientes.html")
+
+def vendedores(request):
+    pass
+
+def vendas(request):
+    pass
+
+def produtos(request):
+    pass
+
+def financeiro(request):
+    pass
+
+def dashboard(request):
+    pass
