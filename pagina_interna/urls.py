4dae3ba600216f63f0d2c774a78736dfea2d7f3e
Seu Bruce <matheusbusch2006@gmail.com>
2026-03-04 18:04:23 -0300

    Paginas de dashboards e relatorios

--- pagina_interna/urls.py
+++ pagina_interna/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('clientes/', views.cadastro_clientes, name="cadastro_clientes"),
+    path('dashboards/', views.dashboards, name="dashboards"),
+    path('relatorios/', views.relatorios, name="relatorios"),
 ]
--- pagina_interna/views.py
+++ pagina_interna/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def cadastro_clientes(request):
     return render(request, "cadastro_clientes.html")
+def dashboards(request):
+    return render(request, "dashboards.html")
+def relatorios(request):
+    return render(request, "relatorios.html")
